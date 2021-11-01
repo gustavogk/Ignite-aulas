3deb3dc1714c23c73b953dae383c707d91ff9341
Gustavo Assuncao <gust.krv@gmail.com>
2021-11-01 15:58:33 -0300

    Modulo de conceitos finalizado

--- reactjs/01-github-explorer/src/App.jsx
+++ reactjs/01-github-explorer/src/App.jsx
@@ -1,5 +1,15 @@
+import { Contador } from './components/Contador'
+import { RepositoryList } from './components/RepositoryList'
+
 import './styles/global.scss'
 
 export function App(){
-    return <h1>Hello world ReactJS</h1>
+    return (
+
+    <>
+    <RepositoryList/>
+    <Contador></Contador>
+    </>
+
+    )
 }
